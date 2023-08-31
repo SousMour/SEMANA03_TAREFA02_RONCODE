20247119a2107a683f7ca58685ed19b63c5cb7f2
SousMour <142849312+SousMour@users.noreply.github.com>
2023-08-30 22:26:23 -0300

    Add files via upload

---
+++ bonus.py
@@ -0,0 +1,4 @@
+tempo=float(input())
+valor=float(input())
+bonus= tempo * valor
+print(f'{bonus:.2f}')
---
+++ preco_com_desconto.py
@@ -0,0 +1,6 @@
+preco=input()
+preco=float(preco)
+preco_com_desconto= preco * 0.90
+preco_com_desconto=round(preco_com_desconto,2)
+preco_com_desconto=float(preco_com_desconto)
+print(preco_com_desconto)
